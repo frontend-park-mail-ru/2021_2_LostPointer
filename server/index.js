9c5ab96d2a83b059daf3a735b33f174fe6861db5
Leonid <belyaevleonid2000@mail.ru>
2021-09-13 16:29:12 +0300

    LOS-9: Сервер исправлен под линтер

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
 
 const host = 'localhost';
 const port = process.env.PORT || 8080;
 
 const requestListener = (request, response) => {
-    const path = request.url === '/' ? '/index.html' : request.url;
-    fs.readFile(`.${path}`, (err, data) => {
-        if (err) {
-            data = "404"
-        }
-        response.write(data);
-        response.end();
-    });
+  const path = request.url === '/' ? '/index.html' : request.url;
+  fs.readFile(`.${path}`, (err, data) => {
+    let responseData = data;
+    if (err) {
+      responseData = '404';
+    }
+    response.write(responseData);
+    response.end();
+  });
 };
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`listening at http://${host}:${port}}`)
-})
+  console.log(`listening at http://${host}:${port}`);
+});
