018ca0e937d82d77b043ab888b56e783fede2473
frbgd <kap17597@yandex.ru>
2021-09-23 09:16:30 +0300

    исправил message для валидации пароля

--- js/signin.js
+++ js/signin.js
@@ -16,9 +16,9 @@ const passwordValidityChecks = [
   {
     isInvalid(input) {
       // eslint-disable-next-line no-bitwise
-      return input.value.length < 8 | input.value.length > 100;
+      return input.value.length < 8;
     },
-    invalidityMessage: 'Password required',
+    invalidityMessage: 'This input needs to be at least 5 characters',
     element: document.querySelector('label[for="password"] .auth-form__input-requirements li:nth-child(1)'),
   },
 ];
