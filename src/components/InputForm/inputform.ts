92b35e7935b64449d1fa161772c896ec0d46d85f
saptded <alexey.kosenko2880@gmail.com>
2021-10-31 23:21:39 +0300

    delete root

--- src/components/InputForm/inputform.ts
+++ src/components/InputForm/inputform.ts
@@ -1,6 +1,7 @@
 import { Component } from 'components/Component/component';
 
 import InputFormTemplate from './inputform.hbs';
+import './inputform.scss'
 
 export class InputFormComponent extends Component<null> {
     constructor(props) {
