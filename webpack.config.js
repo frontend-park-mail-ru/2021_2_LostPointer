5a47eccff52971e2065f46432fa75a2e5a1dde0b
Leonid <belyaevleonid2000@mail.ru>
2021-10-31 14:32:39 +0300

    LOS-70: Фавикон фикс

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
             title: 'LostPointer',
             template: './src/index.html',
             filename: path.join(__dirname, '/dist/index.html'),
-            favicon: path.join(__dirname, '/static/img/favicon.ico'),
+            favicon: path.join(__dirname, '/src/static/img/favicon.ico'),
         }),
         new CleanWebpackPlugin(),
     ],
