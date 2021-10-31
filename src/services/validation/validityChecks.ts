bb73960f7177fca732d735d82094fdb3f2902ffd
Leonid <belyaevleonid2000@mail.ru>
2021-11-01 01:01:21 +0300

    LOS-63: Доразбиты стили, вынесены + исправлены regex

--- src/services/validation/validityChecks.ts
+++ src/services/validation/validityChecks.ts
@@ -1,3 +1,12 @@
+import {
+    LETTER,
+    LETTERS_AND_NUMBERS,
+    NUMBER,
+    SPECIAL_CHARS,
+    UPPERCASE_LETTER,
+    VALID_EMAIL,
+} from 'store/regex';
+
 export class ValidityCheck {
     private invalidityMessage: string;
     private isInvalid: boolean;
@@ -20,16 +29,14 @@ export const nameValidityChecks = [
         (input) => input.value.length < 3
     ),
     new ValidityCheck('Name allows only letters and numbers', (input) => {
-        const illegalCharacters = input.value.match(/[^a-zA-Z0-9]/g);
+        const illegalCharacters = input.value.match(LETTERS_AND_NUMBERS);
         return !input.value || !!illegalCharacters;
     }),
 ];
 
 export const emailValidityChecks = [
     new ValidityCheck('Invalid email address', (input) => {
-        const legalEmail = input.value.match(
-            /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+/g
-        );
+        const legalEmail = input.value.match(VALID_EMAIL);
         return !legalEmail;
     }),
 ];
@@ -45,41 +52,38 @@ export const passwordValidityChecks = [
     ),
     new ValidityCheck(
         'Password requires at least 1 number',
-        (input) => !input.value.match(/[0-9]/g)
+        (input) => !input.value.match(NUMBER)
     ),
     new ValidityCheck(
         'Password requires at least 1 lowercase letter',
-        (input) => !input.value.match(/[a-z]/g)
+        (input) => !input.value.match(LETTER)
     ),
     new ValidityCheck(
         'Password requires at least 1 uppercase letter',
-        (input) => !input.value.match(/[A-Z]/g)
+        (input) => !input.value.match(UPPERCASE_LETTER)
     ),
     new ValidityCheck(
         'Password must contain one of the required special characters',
         // eslint-disable-next-line no-useless-escape
-        (input) => !input.value.match(/[@ !"#$%&'()*+,\-.\/:;<=>?\[\\\]^_]/g)
+        (input) => !input.value.match(SPECIAL_CHARS)
     ),
-    new ValidityCheck(
-        'New password doesn\'t need to match the old one',
-        () => {
-            const oldPasswordInput = document.querySelector(
-                'input[name="old_password"]'
-            ) as HTMLTextAreaElement;
-            if (!oldPasswordInput) {
-                return false;
-            }
-
-            const passwordInput = document.querySelector(
-                'input[name="password"]'
-            ) as HTMLTextAreaElement;
-            const isEmpty = passwordInput.value === '';
-            if (isEmpty) {
-                return false;
-            }
-            return oldPasswordInput.value === passwordInput.value
+    new ValidityCheck("New password doesn't need to match the old one", () => {
+        const oldPasswordInput = document.querySelector(
+            'input[name="old_password"]'
+        ) as HTMLTextAreaElement;
+        if (!oldPasswordInput) {
+            return false;
         }
-    ),
+
+        const passwordInput = document.querySelector(
+            'input[name="password"]'
+        ) as HTMLTextAreaElement;
+        const isEmpty = passwordInput.value === '';
+        if (isEmpty) {
+            return false;
+        }
+        return oldPasswordInput.value === passwordInput.value;
+    }),
 ];
 
 export const confirmPasswordValidityChecks = [
