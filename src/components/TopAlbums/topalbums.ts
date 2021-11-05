4b7619212f3fe910c9af68793cdf57fca6bc2303
Leonid <belyaevleonid2000@mail.ru>
2021-11-06 02:02:35 +0300

    LOS-60: Track/s `ЮЗЕР-ФРЕНДЛИ` шаблонизатор

--- src/components/TopAlbums/topalbums.ts
+++ src/components/TopAlbums/topalbums.ts
@@ -4,18 +4,24 @@ import TopAlbumsTemplate from './topalbums.hbs';
 import { AlbumModel } from 'models/album';
 import './topalbums.scss';
 
-import './topalbums.scss';
-
 interface ITopAlbumsProps {
     albums: Array<AlbumModel>;
 }
 
 export class TopAlbums extends Component<ITopAlbumsProps> {
+    private albums: Array<AlbumModel>;
+
     constructor(props) {
         super(props);
+        this.albums = this.props.albums.reduce((acc, album) => {
+            const alb = album;
+            alb.props.album = !alb.isSingle();
+            acc.push(alb);
+            return acc;
+        }, []);
     }
 
     render() {
-        return TopAlbumsTemplate({ albums: this.props.albums });
+        return TopAlbumsTemplate({ albums: this.albums });
     }
 }
