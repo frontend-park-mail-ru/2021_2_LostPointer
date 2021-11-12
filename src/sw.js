c550ef180e671e8059a9a4f9cf10b4060e6deb82
Leonid <belyaevleonid2000@mail.ru>
2021-11-13 02:43:04 +0300

    pred_fix: Починен сервис воркер

--- src/sw.js
+++ src/sw.js
@@ -22,34 +22,34 @@ const isUrlToCache = (url) => {
     }, null);
 };
 
-// self.addEventListener('fetch', (event) => {
-//     if (navigator.onLine === true) {
-//         if (event.request.method === 'GET' && isUrlToCache(event.request.url)) {
-//             caches.open(version + cacheName).then((cache) => {
-//                 cache.add(event.request.url);
-//             });
-//         } else {
-//             return fetch(event.request);
-//         }
-//     } else {
-//         const regex1 = /^(http[s]?:\/.+\/artist)\/\d+$/gm;
-//         const regex2 = /^(http[s]?:\/.+\/api\/v1\/artist)\/\d+$/gm;
-//         const match1 = regex1.exec(event.request.url);
-//         const match2 = regex2.exec(event.request.url);
-//         if (match1 && !match2) {
-//             event.respondWith(
-//                 caches.match(new Request(match1[1])).then((cachedResponse) => {
-//                     return cachedResponse;
-//                 })
-//             );
-//         } else {
-//             event.respondWith(
-//                 caches.match(event.request).then((cachedResponse) => {
-//                     if (cachedResponse) {
-//                         return cachedResponse;
-//                     }
-//                 })
-//             );
-//         }
-//     }
-// });
+self.addEventListener('fetch', (event) => {
+    if (navigator.onLine === true) {
+        if (event.request.method === 'GET' && isUrlToCache(event.request.url)) {
+            caches.open(version + cacheName).then((cache) => {
+                cache.add(event.request.url);
+            });
+        } else {
+            return null;
+        }
+    } else {
+        const regex1 = /^(http[s]?:\/.+\/artist)\/\d+$/gm;
+        const regex2 = /^(http[s]?:\/.+\/api\/v1\/artist)\/\d+$/gm;
+        const match1 = regex1.exec(event.request.url);
+        const match2 = regex2.exec(event.request.url);
+        if (match1 && !match2) {
+            event.respondWith(
+                caches.match(new Request(match1[1])).then((cachedResponse) => {
+                    return cachedResponse;
+                })
+            );
+        } else {
+            event.respondWith(
+                caches.match(event.request).then((cachedResponse) => {
+                    if (cachedResponse) {
+                        return cachedResponse;
+                    }
+                })
+            );
+        }
+    }
+});
