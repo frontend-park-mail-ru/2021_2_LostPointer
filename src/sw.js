7e61dc016ca47b28a3aeb4b14588da9c8305292e
Leonid <belyaevleonid2000@mail.ru>
2021-11-13 16:31:10 +0300

    arch_refactor: Фикс сервис воркера

--- src/sw.js
+++ src/sw.js
@@ -4,9 +4,11 @@ const cacheName = '::losServiceWorker';
 const version = 'v0.0.1';
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(version + cacheName).then((cache) => cache.addAll(cachedUrl)),
-  );
+    event.waitUntil(
+        caches
+            .open(version + cacheName)
+            .then((cache) => cache.addAll(cachedUrl))
+    );
 });
 
 const isUrlToCache = (url) => {
@@ -14,43 +16,39 @@ const isUrlToCache = (url) => {
         /^http[s]?:\/.+\/static\/\w+\/.+\.webp$/gm,
         /^http[s]?:\/.+\/api\/v1\/artist\/\d+$/gm,
         /^http[s]?:\/.+\/api\/v1\/user\/settings$/gm,
-    ]
+    ];
     return regexes.reduce((prevMatch, regex) => {
         return regex.exec(url) || prevMatch;
-    }, null)
-}
+    }, null);
+};
 
 self.addEventListener('fetch', (event) => {
     if (navigator.onLine === true) {
-        if (event.request.method === 'GET' && isUrlToCache(event.request.url )) {
-            caches.open(version + cacheName)
-                .then((cache) => {
-                    cache.add(event.request.url);
-                });
+        if (event.request.method === 'GET' && isUrlToCache(event.request.url)) {
+            caches.open(version + cacheName).then((cache) => {
+                cache.add(event.request.url);
+            });
         } else {
-            return fetch(event.request)
+            return null;
         }
     } else {
         const regex1 = /^(http[s]?:\/.+\/artist)\/\d+$/gm;
         const regex2 = /^(http[s]?:\/.+\/api\/v1\/artist)\/\d+$/gm;
         const match1 = regex1.exec(event.request.url);
-        const match2 = regex2.exec(event.request.url)
+        const match2 = regex2.exec(event.request.url);
         if (match1 && !match2) {
             event.respondWith(
-                caches.match(new Request(match1[1]))
-                    .then((cachedResponse) => {
-                            return cachedResponse;
-                        })
-            )
+                caches.match(new Request(match1[1])).then((cachedResponse) => {
+                    return cachedResponse;
+                })
+            );
         } else {
             event.respondWith(
-                caches
-                    .match(event.request)
-                    .then((cachedResponse) => {
-                        if (cachedResponse) {
-                            return cachedResponse;
-                        }
-                    })
+                caches.match(event.request).then((cachedResponse) => {
+                    if (cachedResponse) {
+                        return cachedResponse;
+                    }
+                })
             );
         }
     }
