12993a47244e1781125cabdde87beca74db857e0
saptded <alexey.kosenko2880@gmail.com>
2021-11-14 22:11:22 +0300

    Change password validation

--- src/services/validation/validityChecks.ts
+++ src/services/validation/validityChecks.ts
@@ -1,10 +1,7 @@
 import {
-    EMAIL,
-    LOWERCASE_LETTER,
+    EMAIL, LETTER,
     NAME,
     NUMBER,
-    SPECIAL_CHARACTER,
-    UPPERCASE_LETTER,
 } from 'store/regex';
 
 export class ValidityCheck {
@@ -26,7 +23,7 @@ export class ValidityCheck {
 export const nameValidityChecks = [
     new ValidityCheck(
         'Name needs to be at least 3 characters',
-        (input) => input.value.length < 3
+        (input) => input.value.length < 3,
     ),
     new ValidityCheck('Name allows only letters and numbers', (input) => {
         const illegalCharacters = input.value.match(NAME);
@@ -37,7 +34,7 @@ export const nameValidityChecks = [
 export const emailValidityChecks = [
     new ValidityCheck('Invalid email address', (input) => {
         const legalEmail = input.value.match(
-            EMAIL
+            EMAIL,
         );
         return !legalEmail;
     }),
@@ -50,44 +47,35 @@ export const simplePasswordValidityChecks = [
 export const passwordValidityChecks = [
     new ValidityCheck(
         'Password needs to be greater than 8 characters',
-        (input) => input.value.length < 8
+        (input) => input.value.length < 8,
     ),
     new ValidityCheck(
         'Password requires at least 1 number',
-        (input) => !input.value.match(NUMBER)
+        (input) => !input.value.match(NUMBER),
     ),
     new ValidityCheck(
-        'Password requires at least 1 lowercase letter',
-        (input) => !input.value.match(LOWERCASE_LETTER)
-    ),
-    new ValidityCheck(
-        'Password requires at least 1 uppercase letter',
-        (input) => !input.value.match(UPPERCASE_LETTER)
-    ),
-    new ValidityCheck(
-        'Password must contain one of the required special characters',
-        // eslint-disable-next-line no-useless-escape
-        (input) => !input.value.match(SPECIAL_CHARACTER)
+        'Password requires at least 1 letter',
+        (input) => !input.value.match(LETTER),
     ),
     new ValidityCheck(
         'New password doesn\'t need to match the old one',
         () => {
             const oldPasswordInput = document.querySelector(
-                'input[name="old_password"]'
+                'input[name="old_password"]',
             ) as HTMLTextAreaElement;
             if (!oldPasswordInput) {
                 return false;
             }
 
             const passwordInput = document.querySelector(
-                'input[name="password"]'
+                'input[name="password"]',
             ) as HTMLTextAreaElement;
             const isEmpty = passwordInput.value === '';
             if (isEmpty) {
                 return false;
             }
-            return oldPasswordInput.value === passwordInput.value
-        }
+            return oldPasswordInput.value === passwordInput.value;
+        },
     ),
 ];
 
@@ -96,16 +84,16 @@ export const confirmPasswordValidityChecks = [
         'Password confirmation needs to match the password',
         () => {
             const passwordInput = document.querySelector(
-                'input[name="password"]'
+                'input[name="password"]',
             ) as HTMLTextAreaElement;
             const confirmPasswordInput = document.querySelector(
-                'input[name="confirm_password"]'
+                'input[name="confirm_password"]',
             ) as HTMLTextAreaElement;
             const isEmpty = confirmPasswordInput.value === '';
             if (isEmpty) {
                 return false;
             }
             return confirmPasswordInput.value !== passwordInput.value;
-        }
+        },
     ),
 ];
--- src/store/regex.ts
+++ src/store/regex.ts
@@ -2,8 +2,9 @@ export const PATH_SLASH = /\//g;
 export const PATH_ARG = /:\w+/g;
 export const PATH_ARG_CG = /:(\w+)/g;
 export const EMAIL = /^([a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*)(@)([a-zA-Z0-9]+)([.][a-zA-Z0-9]+)?\.([a-zA-Z0-9]+)$/g
-export const NAME = /^([\w]+)|([0-9А-Яа-я_]+)$/g
+export const NAME = /^([\wА-Яа-я]+)$/g
 export const NUMBER = /[0-9]/g
+export const LETTER = /[a-zA-Z]/g
 export const LOWERCASE_LETTER = /[a-z]/g
 export const UPPERCASE_LETTER = /[A-Z]/g
 export const SPECIAL_CHARACTER = /[@ !"#$%&'()*+,\-./:;<=>?[\\\]^_]/g
