b1a9240c4df815eb93e9b9c1bafa2bf205eb602f
frbgd <kap17597@yandex.ru>
2021-11-28 18:39:40 +0300

    playlists_after_rk3: prehandler express => http

--- utils/server.ts
+++ utils/server.ts
@@ -1,6 +1,5 @@
 const fs = require('fs');
-const express = require('express');
-const app = express();
+var http = require('http');
 const path = require('path');
 import { parse } from 'node-html-parser';
 import fetch from 'node-fetch';
@@ -8,118 +7,119 @@ import fetch from 'node-fetch';
 const origin = 'https://lostpointer.site';
 const api = '/api/v1'
 
-const indexFile = fs.readFileSync(path.join(__dirname + '/../dist/index.html'));
+const indexFile = String(fs.readFileSync(path.join(__dirname + '/../dist/index.html')));
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    let regex = /^\/artist\/(\d+)$/gm;
+    let match = regex.exec(req.url);
+    if (match) {
+        const artistId = match[1];
+        fetch(`${origin}${api}/artist/${artistId}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                if (data.message) {
+                    res.sendFile(path.join(__dirname + '/../src/index.html'));
+                    return;
+                }
+
+                const document = parse(indexFile);
+                document.querySelector('meta[property="og:title"]')
+                    .setAttribute('content', data.name);
+                document.querySelector('meta[property="og:image"]')
+                    .setAttribute(
+                        'content',
+                        `${origin}${data.avatar}`
+                    );
+                document.querySelector('meta[property="og:url"]')
+                    .setAttribute('content', `${origin}${req.path}`);
+                document.querySelector('meta[property="og:description"]')
+                    .setAttribute('content', '');
+
+                res.write(document.toString());
+                res.end();
+            })
+            .catch((error) => {
+                res.write(indexFile);
+                res.end();
+            });
+    }
+    regex = /^\/album\/(\d+)$/gm;
+    match = regex.exec(req.url);
+    if (match) {
+        const albumId = match[1];
+        fetch(`${origin}${api}/album/${albumId}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                if (data.message) {
+                    res.sendFile(path.join(__dirname + '/../src/index.html'));
+                    return;
+                }
+
+                const document = parse(indexFile);
+                document.querySelector('meta[property="og:title"]')
+                    .setAttribute('content', `${data.artist.name} - ${data.title}`);
+                document.querySelector('meta[property="og:image"]')
+                    .setAttribute(
+                        'content',
+                        `${origin}/static/artworks/${data.artwork}_512px.webp`
+                    );
+                document.querySelector('meta[property="og:url"]')
+                    .setAttribute('content', `${origin}${req.path}`);
+                document.querySelector('meta[property="og:description"]')
+                    .setAttribute('content', '');
+
+                res.write(document.toString());
+                res.end();
+            })
+            .catch((error) => {
+                res.write(indexFile);
+                res.end();
+            });
+    }
+    regex = /^\/playlist\/(\d+)$/gm;
+    match = regex.exec(req.url);
+    if (match) {
+        const playlistId = match[1];
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../src/index.html'));
+        fetch(`${origin}${api}/playlist/${playlistId}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                if (data.message) {
+                    res.sendFile(path.join(__dirname + '/../src/index.html'));
+                    return;
+                }
+
+                const document = parse(indexFile);
+                document.querySelector('meta[property="og:title"]')
+                    .setAttribute('content', `Playlist: ${data.title}`);
+                document.querySelector('meta[property="og:image"]')
+                    .setAttribute(
+                        'content',
+                        `${origin}${data.artwork}`
+                    );
+                document.querySelector('meta[property="og:url"]')
+                    .setAttribute('content', `${origin}${req.path}`);
+                document.querySelector('meta[property="og:description"]')
+                    .setAttribute('content', '');
+
+                res.write(document.toString());
+                res.end();
+            })
+            .catch((error) => {
+                res.write(indexFile);
+                res.end();
+            });
+    }
+
+}).listen(8888, function(){
+    console.log("server start at port 8888"); //the server object listens on port 3000
 });
-
-app.get(/^\/artist\/(\d+)$/gm, (req, res) => {
-    const regex = /^\/artist\/(\d+)$/gm;
-    const match = regex.exec(req.path);
-    const artistId = match[1];
-
-
-    fetch(`${origin}${api}/artist/${artistId}`)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            if (data.message) {
-                res.sendFile(path.join(__dirname + '/../src/index.html'));
-                return;
-            }
-
-            const document = parse(String(indexFile));
-            document.querySelector('meta[property="og:title"]')
-                .setAttribute('content', data.name);
-            document.querySelector('meta[property="og:image"]')
-                .setAttribute(
-                    'content',
-                    `${origin}${data.avatar}`
-                );
-            document.querySelector('meta[property="og:url"]')
-                .setAttribute('content', `${origin}${req.path}`);
-            document.querySelector('meta[property="og:description"]')
-                .setAttribute('content', '');
-
-            res.send(document.toString());
-        })
-        .catch((error) => {
-            res.sendFile(path.join(__dirname + '/../src/index.html'));
-        });
-});
-
-app.get(/^\/album\/(\d+)$/gm, (req, res) => {
-    const regex = /^\/album\/(\d+)$/gm;
-    const match = regex.exec(req.path);
-    const albumId = match[1];
-
-
-    fetch(`${origin}${api}/album/${albumId}`)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            if (data.message) {
-                res.sendFile(path.join(__dirname + '/../src/index.html'));
-                return;
-            }
-
-            const document = parse(String(indexFile));
-            document.querySelector('meta[property="og:title"]')
-                .setAttribute('content', `${data.artist.name} - ${data.title}`);
-            document.querySelector('meta[property="og:image"]')
-                .setAttribute(
-                    'content',
-                    `${origin}/static/artworks/${data.artwork}_512px.webp`
-                );
-            document.querySelector('meta[property="og:url"]')
-                .setAttribute('content', `${origin}${req.path}`);
-            document.querySelector('meta[property="og:description"]')
-                .setAttribute('content', '');
-
-            res.send(document.toString());
-        })
-        .catch((error) => {
-            res.sendFile(path.join(__dirname + '/../src/index.html'));
-        });
-});
-app.get(/^\/playlist\/(\d+)$/gm, (req, res) => {
-    const regex = /^\/playlist\/(\d+)$/gm;
-    const match = regex.exec(req.path);
-    const playlistId = match[1];
-
-
-    fetch(`${origin}${api}/playlist/${playlistId}`)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            if (data.message) {
-                res.sendFile(path.join(__dirname + '/../src/index.html'));
-                return;
-            }
-
-            const document = parse(String(indexFile));
-            document.querySelector('meta[property="og:title"]')
-                .setAttribute('content', `Playlist: ${data.title}`);
-            document.querySelector('meta[property="og:image"]')
-                .setAttribute(
-                    'content',
-                    `${origin}${data.artwork}`
-                );
-            document.querySelector('meta[property="og:url"]')
-                .setAttribute('content', `${origin}${req.path}`);
-            document.querySelector('meta[property="og:description"]')
-                .setAttribute('content', '');
-
-            res.send(document.toString());
-        })
-        .catch((error) => {
-            res.sendFile(path.join(__dirname + '/../src/index.html'));
-        });
-});
-
-app.listen(8888);
